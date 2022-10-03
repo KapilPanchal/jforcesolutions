fc81394a0fb131cab8b3bc4fd6acf756adcb9471
kapil-panchal <kapil.panchal@nouvelletechnology.com.au>
2022-10-03 10:50:28 +0530

    BASE APP: Added aws-beanstack-demo project

---
+++ aws-beanstack-demo/src/main/java/com/app63/awsbeanstackdemo/AwsBeanstackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.app63.awsbeanstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBeanstackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBeanstackDemoApplication.class, args);
+	}
+
+}
---
+++ aws-beanstack-demo/src/test/java/com/app63/awsbeanstackdemo/AwsBeanstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app63.awsbeanstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsBeanstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
